a9475c210f0de4f3f652d84c2567b20bfad7c6df
Urkel <abmichael109@gmail.com>
2025-06-18 19:13:07 +0100

    new

--- src/pages/admin/tools/index.tsx
+++ src/pages/admin/tools/index.tsx
@@ -1,11 +1,13 @@
 import { getTemplates } from "@/api/apiEndpoints";
 import ToolCard from "@/components/Admin/Tools/ToolCard";
+import { Skeleton } from "@/components/ui/skeleton";
+import type { Template } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 import { Plus } from "lucide-react";
 import { Link } from "react-router-dom";
 
 export default function Tools() {
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery<Template[]>({
     queryFn: getTemplates,
     queryKey: ["templates"]
   })
@@ -27,6 +29,11 @@ export default function Tools() {
           <ToolCard key={tool.id} tool={tool} />
         ))}
       </div>
+      {isLoading  && <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+       {Array.from({ length: 6 }).map((_, index) => (
+        <Skeleton key={index} className="w-full h-[200px] bg-white/10 rounded-xl" />
+       ))}
+      </div>}
     </div>
   );
 }
