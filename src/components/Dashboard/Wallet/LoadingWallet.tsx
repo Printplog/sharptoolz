f1ab9f11ecfc21f045163d6cdca40e7ad775a5ac
Urkel <abmichael109@gmail.com>
2025-07-03 15:17:08 +0100

    new changes

--- src/components/Dashboard/Wallet/LoadingWallet.tsx
+++ src/components/Dashboard/Wallet/LoadingWallet.tsx
@@ -3,7 +3,16 @@ import { Skeleton } from '@/components/ui/skeleton'
 export default function LoadingWallet() {
   return (
     <div className='space-y-6'>
-        <Skeleton className="bg-white/5 border border-white/10" />
+        <div className="flex justify-between items-center">
+            <Skeleton className="bg-white/5 border border-white/10 h-8 w-[80px]" />
+            <Skeleton className="bg-white/5 border border-white/10 h-10 w-[100px]" />
+        </div>
+        <Skeleton className="bg-white/5 border border-white/10 h-30" />
+        <div className="space-y-4 bg-white/5 border border-white/10 p-4 rounded-lg">
+            {Array.from({ length: 8 }).map((_, index) => (
+                <Skeleton key={index} className="bg-white/10 border border-white/20 h-12" />
+            ))}
+        </div>
     </div>
   )
 }
