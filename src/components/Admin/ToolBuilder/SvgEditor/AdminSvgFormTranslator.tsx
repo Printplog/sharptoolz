04edf3f37acc118e3e1e00ffa3af1bc6145f2e78
Michael <abmichael109@gmail.com>
2025-10-27 11:10:54 +0100

    hey

--- src/components/Admin/ToolBuilder/SvgEditor/AdminSvgFormTranslator.tsx
+++ src/components/Admin/ToolBuilder/SvgEditor/AdminSvgFormTranslator.tsx
@@ -10,7 +10,7 @@ interface AdminSvgFormTranslatorProps {
   svgContent: string;
   formFields: FormField[];
   templateName: string;
-}
+} 
 
 export default function AdminSvgFormTranslator({ 
   svgContent, 
