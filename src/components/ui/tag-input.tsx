8f64fa67f11544624a7575ce2523449a6867442f
Michael <abmichael109@gmail.com>
2025-11-12 23:22:22 +0100

    Remove unused Input import from TagInput component to improve code clarity.

--- src/components/ui/tag-input.tsx
+++ src/components/ui/tag-input.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { X } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { Input } from "./input";
 
 interface TagInputProps {
   tags: string[];
