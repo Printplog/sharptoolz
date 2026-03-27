316eac48da5e75a58d5a3d43bc92fb2801ad10bc
Abmichael01 <abmichael109@gmail.com>
2026-03-27 10:40:53 +0100

    refactor: adjust Hero component heading width, text wrapping, and highlighting.

--- src/components/Site/Home/Hero.tsx
+++ src/components/Site/Home/Hero.tsx
@@ -6,12 +6,12 @@ import { PremiumButton } from "@/components/ui/PremiumButton";
 
 export default function Hero() {
   return (
-    <SectionPadding className="flex flex-col items-center justify-center text-center pt-12 md:pt-12 relative overflow-hidden">
+    <SectionPadding className="flex flex-col items-center justify-center text-center pt-12 md:pt-12 relative">
       <LightBlur />
-      <h2 className="text-[38px] sm:text-6xl md:text-7xl font-fancy text-center font-semibold leading-[1.1] tracking-tight">
-        Create Professional and Realistic Sample <br />
+      <h2 className="text-[38px] sm:text-6xl md:text-7xl max-w-4xl font-fancy text-center font-semibold leading-[1.1] tracking-tight">
+        Create Professional and Realistic Sample {' '}
         <span className="text-[#cee88c] drop-shadow-[0_0_15px_rgba(206,232,140,0.3)]">
-          Documents in Seconds
+           Documents in Seconds
         </span>.
       </h2>
       <p className="text-white/60 text-base md:text-lg max-w-2xl mt-6 font-medium">
