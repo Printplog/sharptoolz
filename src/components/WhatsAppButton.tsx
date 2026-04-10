1cfa12cf7d953e789bbb0ebb5f1350d2e119903b
Abmichael01 <abmichael109@gmail.com>
2026-04-10 14:25:33 +0100

    feat: finalize social links in floating menu and remove email

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -9,7 +9,9 @@ import {
   X, 
   Headphones, 
   Send, 
-  Mail,
+  Twitter,
+  Instagram,
+  Video
 } from "lucide-react";
 
 interface ActionItem {
@@ -27,6 +29,16 @@ const WhatsAppIcon = ({ className }: { className?: string }) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413z"/>
+  </svg>
+);
+
+const TikTokIcon = ({ className }: { className?: string }) => (
+  <svg 
+    viewBox="0 0 24 24" 
+    className={cn("w-5 h-5 fill-current", className)} 
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M12.525.02c1.31-.32 2.51.85 2.51 2.1l-.01 6.31c1.33-1.25 3.03-1.6 4.79-1.03l.01 2.92a4.99 4.99 0 0 0-3.32.33l-.01 7.82c0 3.1-2.52 5.62-5.63 5.62-3.11 0-5.63-2.52-5.63-5.63 0-3.1 2.52-5.62 5.63-5.62l.01 3.01c-1.45 0-2.62 1.17-2.62 2.62s1.17 2.62 2.62 2.62c1.45 0 2.63-1.17 2.63-2.62l.01-18.15Z"/>
   </svg>
 );
 
@@ -103,17 +115,31 @@ export default function WhatsAppButton() {
     },
     {
       id: "telegram",
-      label: "Telegram Support",
+      label: "Telegram",
       icon: <Send size={20} />,
       href: settings?.telegram_link || "#",
       color: "bg-[#0088cc]",
     },
     {
-      id: "email",
-      label: "Email Support",
-      icon: <Mail size={20} />,
-      href: settings?.support_email ? `mailto:${settings.support_email}` : "#",
-      color: "bg-indigo-500",
+      id: "instagram",
+      label: "Instagram",
+      icon: <Instagram size={20} />,
+      href: settings?.instagram_link || "#",
+      color: "bg-radial-[at_30%_107%] from-[#fdf497] via-[#fdf497] to-[#d6249f] shadow-lg",
+    },
+    {
+      id: "twitter",
+      label: "Twitter (X)",
+      icon: <Twitter size={20} />,
+      href: settings?.twitter_link || "#",
+      color: "bg-black",
+    },
+    {
+      id: "tiktok",
+      label: "TikTok",
+      icon: <TikTokIcon />,
+      href: settings?.tiktok_link || "#",
+      color: "bg-black border border-white/10",
     },
   ].filter(action => action.href !== "#");
 
