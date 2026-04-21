8315c4d0565a64dbd82f711f84e154067a24e88f
Abmichael01 <abmichael109@gmail.com>
2026-04-21 12:08:18 +0100

    Fix(build): Remove unused GlobalAnnouncement import

--- src/pages/(site)/_layout.tsx
+++ src/pages/(site)/_layout.tsx
@@ -1,7 +1,6 @@
 import RegisterDialog from '@/components/Auth/RegisterDialog'
 import Footer from '@/components/Site/Footer'
 import Navbar from '@/components/Site/Navbar'
-import GlobalAnnouncement from "@/components/Dashboard/Layouts/GlobalAnnouncement";
 import useDialog from '@/hooks/useDialog'
 import { Outlet } from 'react-router-dom'
 import TermsAgreementDialog from '@/components/Site/TermsAgreementDialog'
